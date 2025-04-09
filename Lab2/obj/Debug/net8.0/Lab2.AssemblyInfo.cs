6940c236a59af387ff20266e50f6fc3b78e91b6a
Ahmed Mohamed <46195866+AhmedMAly4@users.noreply.github.com>
2025-04-09 01:12:21 +0000

    Lab 4 Complete

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81568392d4348d16e8694f9ff71cda7f9e7a77fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34643f948b779512450df30f4ece432790dfeaa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab4/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple For Loop (1 to 10)");
+            Problem1();
+            Console.WriteLine();
+
+            Console.WriteLine("Even Numbers from 1 to 20");
+            Problem2();
+            Console.WriteLine();
+
+            Console.WriteLine("While Loop Countdown (5 to 1)");
+            Problem3();
+            Console.WriteLine();
+
+            Console.WriteLine("Do/While User Input (Number > 100)");
+            Problem4();
+            Console.WriteLine();
+
+            Console.WriteLine("Multiples of 10 from 10 to 1000");
+            Problem5();
+            Console.WriteLine();
+
+            Console.WriteLine("Pattern Printer");
+            Problem6();
+        }
+
+        static void Problem1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Problem2()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static void Problem3()
+        {
+            int count = 5;
+            while (count >= 1)
+            {
+                Console.WriteLine(count);
+                count--;
+            }
+        }
+
+        static void Problem4()
+        {
+            int number;
+            do
+            {
+                Console.Write("Enter a number greater than 100: ");
+                number = Convert.ToInt32(Console.ReadLine());
+            } while (number <= 100);
+            
+            Console.WriteLine($"You entered: {number}");
+        }
+
+        static void Problem5()
+        {
+            int num = 10;
+            while (num <= 1000)
+            {
+                Console.WriteLine(num);
+                num += 10;
+            }
+        }
+
+        static void Problem6()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
